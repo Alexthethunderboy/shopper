fb1fe24fb97dfc3735a77e360ad600468bd945c6
Alexthethunderboy <khaylex2.0@gmail.com>
2025-01-23 10:58:32 +0000

    mainfix

--- next.config.js
+++ next.config.js
@@ -4,29 +4,19 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
+        protocol: "https",
+        hostname: "**",
       },
     ],
-  },
-  typescript: {
-    // Dangerously allow production builds to successfully complete even if your project has type errors
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if your project has ESLint errors
-    ignoreDuringBuilds: true,
   },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, './src'),
-    };
-    return config;
-  },
-  experimental: {
-    esmExternals: 'loose',
+      "@": require("path").resolve(__dirname, "./src"),
+    }
+    return config
   },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
+
