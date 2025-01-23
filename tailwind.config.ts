542c58ece3b51709c90f0b824cd18b147d8d88a9
Alexthethunderboy <khaylex2.0@gmail.com>
2025-01-23 10:26:37 +0000

    buttonfix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -65,45 +65,57 @@ const config = {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		keyframes: {
-  			"accordion-down": {
-  				from: { height: "0" },
-  				to: { height: "var(--radix-accordion-content-height)" },
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
   			},
-  			"accordion-up": {
-  				from: { height: "var(--radix-accordion-content-height)" },
-  				to: { height: "0" },
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
   			},
   			shimmer: {
-  				"0%": { backgroundPosition: "100% 50%" },
-  				"100%": { backgroundPosition: "0 50%" },
+  				'0%': {
+  					backgroundPosition: '100% 50%'
+  				},
+  				'100%': {
+  					backgroundPosition: '0 50%'
+  				}
   			},
-  			"fade-up": {
+  			'fade-up': {
   				from: {
-  					opacity: "0",
-  					transform: "translateY(20px)",
+  					opacity: '0',
+  					transform: 'translateY(20px)'
   				},
   				to: {
-  					opacity: "1",
-  					transform: "translateY(0)",
-  				},
+  					opacity: '1',
+  					transform: 'translateY(0)'
+  				}
   			},
-  			"fade-down": {
+  			'fade-down': {
   				from: {
-  					opacity: "0",
-  					transform: "translateY(-20px)",
+  					opacity: '0',
+  					transform: 'translateY(-20px)'
   				},
   				to: {
-  					opacity: "1",
-  					transform: "translateY(0)",
-  				},
-  			},
+  					opacity: '1',
+  					transform: 'translateY(0)'
+  				}
+  			}
   		},
   		animation: {
-  			"accordion-down": "accordion-down 0.2s ease-out",
-  			"accordion-up": "accordion-up 0.2s ease-out",
-  			shimmer: "shimmer 1.4s ease infinite",
-  			"fade-up": "fade-up 0.5s ease-out forwards",
-  			"fade-down": "fade-down 0.5s ease-out forwards",
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+  			shimmer: 'shimmer 1.4s ease infinite',
+  			'fade-up': 'fade-up 0.5s ease-out forwards',
+  			'fade-down': 'fade-down 0.5s ease-out forwards'
   		}
   	}
   },
