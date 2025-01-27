5e8c964e18d03c5f7267108874ae28d213fd518e
Alexthethunderboy <khaylex2.0@gmail.com>
2025-01-27 19:28:54 +0000

    at llastttt

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,29 +1,76 @@
-export { Button, buttonVariants } from './button';
-export { Input } from './input';
-export { Card, CardContent, CardFooter } from './card';
-export { Dialog, DialogContent, DialogTrigger } from './dialog';
-export { toast } from './toast';
-export { Checkbox } from './checkbox';
-export { Avatar, AvatarFallback, AvatarImage } from './avatar';
-export { Badge } from './badge';
-export { Carousel } from './carousel';
-export { Command } from './command';
-export { Drawer } from './drawer';
-export { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './dropdown-menu';
-export { Form } from './form';
-export { Label } from './label';
-export { Modal } from './modal';
-export { ScrollArea } from './scroll-area';
-export { Select } from './select';
-export { Separator } from './separator';
-export { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from './sheet';
-export { Slider } from './slider';
-export { Tabs } from './tabs';
-export { ThemeToggle } from './theme-toggle';
-export { Toaster } from './toaster';
-export { QuickView } from './quick-view';
-export { ProductSkeleton } from './product-skeleton';
-export { ProductCardSkeleton } from './product-card-skeleton';
-export { LoadingSpinner } from './loading-spinner';
-export { ScrollAnimation } from './scroll-animation';
-export { ImageGallery } from './image-gallery'; 
+import { Button, buttonVariants } from "../button";
+import { Input } from "./input";
+import { Card, CardContent, CardFooter } from "./card";
+import { Dialog, DialogContent, DialogTrigger } from "./dialog";
+import { Toast } from "./toast";
+import { Checkbox } from "./checkbox";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Badge } from "./badge";
+import { Carousel } from "./carousel";
+import { Command } from "./command";
+import { Drawer } from "./drawer";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./dropdown-menu";
+import { Form } from "./form";
+import { Label } from "./label";
+import { Modal } from "./modal";
+import { ScrollArea } from "./scroll-area";
+import { Select } from "./select";
+import { Separator } from "./separator";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./sheet";
+import { Slider } from "./slider";
+import { Tabs } from "./tabs";
+import { ThemeToggle } from "./theme-toggle";
+import { Toaster } from "./toaster";
+import { QuickView } from "./quick-view";
+import { ProductSkeleton } from "./product-skeleton";
+import { ProductCardSkeleton } from "./product-card-skeleton";
+import { LoadingSpinner } from "./loading-spinner";
+import { ScrollAnimation } from "./scroll-animation";
+import { ImageGallery } from "./image-gallery";
+
+export {
+  Button,
+  buttonVariants,
+  Input,
+  Card,
+  CardContent,
+  CardFooter,
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  Toast,
+  Checkbox,
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Badge,
+  Carousel,
+  Command,
+  Drawer,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  Form,
+  Label,
+  Modal,
+  ScrollArea,
+  Select,
+  Separator,
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+  Slider,
+  Tabs,
+  ThemeToggle,
+  Toaster,
+  QuickView,
+  ProductSkeleton,
+  ProductCardSkeleton,
+  LoadingSpinner,
+  ScrollAnimation,
+  ImageGallery,
+}; 
